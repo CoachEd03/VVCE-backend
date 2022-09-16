91fa020b519854fe27ad78d986c5103e4062efe3
Shrisha2001 <shrisha.p2001@gmail.com>
2022-09-16 19:08:18 +0530

    modified:   frontend/src/components/Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,6 +1,13 @@
 function Register() {
     return (
-      <form method="POST"></form>);
+      <form method="POST">
+        <b>Register form</b><br/>
+        <label> First Name: <input type="text" name="fname" required/>
+        </label><br/>
+        <label> Last Name: <input type="text" name="lname" required/>
+        </label><br/>
+        <input type="submit" value="Submit" />
+    </form>);
   }
   
 export default Register;
