c556e1456f36b53a3bc66b571d7c7b32785cc0ed
CoachEd03 <113235724+CoachEd03@users.noreply.github.com>
2022-09-21 13:44:44 +0530

    Changes for router and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import Register from "./components/Register";
+import Login from "./components/Login";
+import Reservation from "./components/Reservations";
+import Dashboard from "./Dashboard";
+import Navbar from "./Navbar";
+
+import { Route, Routes } from "react-router-dom";
+import "./styles.scss";
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>);
+    <>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reservation" element={<Reservation />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
