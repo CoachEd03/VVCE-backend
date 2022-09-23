a5b24783ae293bce9ef120ab37f2f0adad340016
suryamanojp <suryamanojp14@gmail.com>
2022-09-23 23:24:04 +0530

    about us page change

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -24,19 +24,10 @@ export default function Home() {
         </h1>
         <div className="AboutContainer__blackline"></div>
         <p className="AboutContainer__paragraph">
-          "At vero eos et accusamus et iusto odio dignissimos ducimus qui
-          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-          et quas molestias excepturi sint occaecati cupiditate non provident,
-          similique sunt in culpa qui officia deserunt mollitia animi, id est
-          laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
-          distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
-          cumque nihil impedit quo minus id quod maxime placeat facere possimus,
-          omnis voluptas assumenda est, omnis dolor repellendus. Temporibus
-          autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe
-          eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
-          Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-          voluptatibus maiores alias consequatur aut perferendis doloribus
-          asperiores repellat."
+        As a restaurant operator, you have to wear many different hats, even if the responsibilities you take on are not within your expertise. Writing is one of those tasks. It can be intimidating to write something customers find interesting, whether it be creating a new email campaign or a website’s About Us section. 
+However, the about page on your site is particularly important for online visitors. Customers are naturally curious, and not just about your menu. An about page tells the story of a business, highlighting what makes a restaurant unique. Everyone loves a good origin story!
+Menufy builds free brand-based online ordering websites for our restaurant partners, including an About Us section. Our associates, such as Erin Trampel, a senior graphic designer, have experience helping our clients highlight interesting and informative content to include on their websites. 
+“An About Us section helps your customers connect more with your restaurant and grow an attachment to it,” Trampel explains.
         </p>
         <button className="AboutContainer__ReadMorebutton">Read More</button>
         <div className="AboutContainer__Redline"></div>
