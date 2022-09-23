d23f1fe17fcfc2fd1a9f30d3b4ffa2abf104c4fa
suryamanojp <suryamanojp14@gmail.com>
2022-09-23 22:48:40 +0530

    reservations created

--- frontend/src/components/Reservations.js
+++ frontend/src/components/Reservations.js
@@ -1,53 +1,95 @@
-import {useState} from 'react'
-function CreateMeal() {
-    const[selection,setSelection]= useState ({ 
-        mealName: "",
-        add_ingredients:"",
-        type:"",
-        price:"",
-        courseMeal:""
-    })
-    function Continue(e) {
-        e.preventDefault()
-        console.log("Creating the best meal option!")
-    }
-    return (
-        <div>
-        <form onClick={Continue}>
-            
-            <b>MEAL SELECTION FORM</b><br/>
-            <label> Meal Name: <input type="text" name={selection.mealName} required/>
-            </label><br/>
-
-            <label> Ingredients:</label><br/>
-             <input type="checkbox" name="ing1" value="chilly"/><br/>
-             <input type="checkbox" name="ing2" required/>fish<br/>
-             <input type="checkbox" name={selection.add_ingredients} required/>mushroom<br/>
-             <input type="checkbox" name={selection.add_ingredients} required/>paneer<br/>
-             <input type="checkbox" name={selection.add_ingredients} required/>chicken<br/>
-             <input type="checkbox" name={selection.add_ingredients} required/>mutton<br/>
-             <input type="checkbox" name={selection.add_ingredients} required/>garlic<br/>
-             <input type="checkbox" name={selection.add_ingredients} required/>egg<br/>
-            
-            <label>Type of meal:</label><br/>
-               <select name={selection.type}>
-                <option>Veg</option>
-                <option>Non-Veg</option>
-               </select><br/>
-            
-            <label>Course Meal:</label><br/>
-               <select name={selection.courseMeal}>
-                <option>Appetizers</option>
-                <option>Starters</option>
-                <option>Main course</option>
-                <option>Dessert</option>
-               </select><br/>
-
-            <label>Price:<input type="number" name={selection.price} required/></label><br/>
-            
-            <input type="submit" value="CONTINUE" />
-        </form>
-        </div>);
+import React, { useState } from "react";
+import "./Home.scss";
+function Reservation() {
+  const [reservation, setReservation] = useState({
+    numberofguests: "",
+    Guestsname: "",
+    Phonenumber: "",
+    Email: "",
+  });
+  function saveSubmit(e) {
+    e.preventDefault();
+      console.log(reservation);
+      alert("Different passwords")
+    // front-end api will be called here and register will be passed as body.
   }
+  return (
+    <div className="reg">
+      <form onSubmit={(e) => saveSubmit(e)}>
+        <b>Reservation form</b>
+        <br />
+        <label>
+          {" "}
+          numberofguests:{" "}
+          <input
+            type="Number"
+            name="numberofguest"
+            value={reservation.numberofguests}
+            onChange={(e) =>
+              setReservation({ ...reservation, numberofguests: e.target.value })
+            }
+            required
+          />
+        </label>
+        <br />
+        <label>
+          {" "}
+          Guestsname:{" "}
+          <input
+            type="String"
+            name="Guestsname"
+            value={reservation.Guestsname}
+            onChange={(e) =>
+              setReservation({ ...reservation, Guestsname: e.target.value })
+            }
+            required
+          />
+        </label>
+        <br />
+        <label>
+          {" "}
+          Email:{" "}
+          <input
+            type="email"
+            name="email"
+            value={reservation.Email}
+            onChange={(e) =>
+              setReservation({ ...reservation, Email: e.target.value })
+            }
+            required
+          />
+        </label>
+        <br />
+        <label>
+          {" "}
+          Phonenumber :{" "}
+          <input
+            type="Number"
+            name="Phonenumber"
+            value={reservation.phno}
+            onChange={(e) => setRegister({ ...register, phno: e.target.value })}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          {" "}
+          Password:{" "}
+          <input
+            type="password"
+            name="pass"
+            value={register.pass}
+            onChange={(e) => setRegister({ ...register, pass: e.target.value })}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit" value="Submit">
+          Reservation
+        </button>
+      </form>
+    </div>
+  );
+}
 
-export default CreateMeal;
+export default Reservation;
