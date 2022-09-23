38e873486068e89874ec16653c997fefda919380
CoachEd03 <113235724+CoachEd03@users.noreply.github.com>
2022-09-23 21:53:27 +0530

    Implemented module

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,15 @@
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const express = require("express");
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+
+import router from "./routes/routes.js";
 const app = express();
 const port = 5001;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.post("/register", (req, res) => {
-  res.send("Received Register");
-});
 
-app.post("/login", (req, res) => {
-  res.send("Received login");
-});
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
