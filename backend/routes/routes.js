ffe7e5d7c8a145c37e8944e115b7fdc34846bc20
CoachEd03 <113235724+CoachEd03@users.noreply.github.com>
2022-10-01 16:07:48 +0530

    new file -updated routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,32 +1,29 @@
 import express from "express";
 
-import ReservationSchema from "../mongo/mongoSchema.js";
+import ReservationSchema from "../mongo/reservationSchema.js";
 
-import Register from "../mongo/mongoSchema.js"
+import Register from "../mongo/registerSchema.js";
 
 const router = express.Router();
 
 router.post("/register", (req, res) => {
-  console.log("\nRegister page : ");
-  console.log(req.body);
-  if(req.body.phno.length == 10 ) {
-    Register.find({email : req.body.email}, function (err, docs) {
-      if (err){
-          console.log(err);
-      }
-      else{
-        if(docs.length == 0) {
+  if (req.body.phno.length == 10) {
+    Register.find({ email: req.body.email }, function (err, docs) {
+      if (err) {
+        console.log(err);
+        res.send("Something went wrong" + err);
+      } else {
+        if (docs.length == 0) {
           const n = new Register(req.body);
           n.save();
           res.send("Register successfull ");
-        }
-        else{
+        } else {
           res.send("Email exists");
         }
-      }});
-  }
-  else {
-    res.send("invalid phone number")
+      }
+    });
+  } else {
+    res.send("invalid phone number");
   }
 });
 
@@ -34,45 +31,55 @@ router.post("/login", (req, res) => {
   console.log("\nLogin page : ");
   console.log(req.body);
   let resu = "";
-  Register.find({email : req.body.email}, function (err, docs) {
-    if (err){
-        console.log(err);
-    }
-    else{
-        console.log("First function call : ", docs);
-        if(docs.length == 0) {
-          console.log("invalid email");
-          resu = "invalid email";
-        }
-        else { 
-          if(docs[0].pass === req.body.pass) {
-            console.log("login successfull");
-            resu = "login successfull";
-        }
-        else {
+  Register.find({ email: req.body.email }, function (err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("First function call : ", docs);
+      if (docs.length == 0) {
+        console.log("invalid email");
+        resu = "invalid email";
+      } else {
+        if (docs[0].pass === req.body.pass) {
+          console.log("login successfull");
+          resu = "login successfull";
+        } else {
           console.log("login password error");
           resu = "login password error";
         }
       }
     }
     res.send(resu);
-});
-//   Register.find({email: req.body.email}, function (err, data) {
-//     if (!err) {
-//         res.render("retrieve", { email: req.body.email });
-//     } else {
-//       res.render("error");
-//     }
-// }).clone().catch(function(err){ console.log(err)})
-});
-
-router.post("/rReservation", (req, res) => {
-  console.log("/nReservation Page : ");
-  console.log(req.body);
-  const n = new ReservationSchema(req.body);
-  n.save();
-  res.send("Received Reservation");
+  });
+  //   Register.find({email: req.body.email}, function (err, data) {
+  //     if (!err) {
+  //         res.render("retrieve", { email: req.body.email });
+  //     } else {
+  //       res.render("error");
+  //     }
+  // }).clone().catch(function(err){ console.log(err)})
 });
 
+router.post("/reservation", (req, res) => {
+  const newReservation = new ReservationSchema(req.body);
+  Register.find({ Email: req.body.email }, function (err, docs) {
+    console.log(docs.length);
+    if (!docs.length) {
+      newReservation.save((err, data) => {
+        console.log("Analyzing Data...");
+        if (data) {
+          console.log("Your data has been successfully saved.");
+          res.send("Successful booking");
+        } else {
+          console.log("Something went wrong while saving data.");
+          console.log(err);
+          res.status(404).send("Something went wrong while saving data.");
+        }
+      });
+    } else {
+      res.status(404).send("The booking from this email already exist");
+    }
+  });
+});
 
 export default router;
