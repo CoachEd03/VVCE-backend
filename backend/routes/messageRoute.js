f19de767b2344d1f8b7ced680e2e9c9cd59a5e80
CoachEd03 <113235724+CoachEd03@users.noreply.github.com>
2022-10-02 20:16:59 +0530

    routes template

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -6,26 +6,18 @@ const router = express.Router();
 router.post("/postMessages", (request, response) => {
   const { title, message } = request.body;
   console.log(title, message);
-  new messageModel({ title, message }).save((err, docs) => {
-    if (err) response.send(" Error in saving data " + err);
-    else response.send("Data saved");
-  });
+  response.send("postMessages")
 });
 
 router.get("/getMessages", (request, response) => {
-  messageModel.find((err, docs) => {
-    if (err) response.send(" Error in saving data " + err);
-    else response.send(docs);
-  });
+  console.log(request.body)
+  response.send("getMessages")
 });
 
 router.get("/updateMessages", (request, response) => {
   const { id, data } = request.body;
   console.log(id, data);
-  messageModel.findByIdAndUpdate(id, data, (err, docs) => {
-    if (err) response.send(" Error in saving data " + err);
-    else response.send(docs);
-  });
+   response.send("updateMessages")
 });
 
 export default router;
