ca35a4c6b9311f1f8542eebf438832f691726dea
SuloniPraveen <sulonipraveen@gmail.com>
2022-10-02 22:31:22 +0530

    edited frontend

--- frontend/src/components/meal.js
+++ frontend/src/components/meal.js
@@ -1,14 +1,25 @@
 import {useState} from 'react'
 //import "./meal_style.scss";
-function CreateMeal() {
+export default function CreateMeal() {
+    const ingredientList =[
+      "Chicken",
+      "Mutton",
+      "Paneer",
+      "fish",
+      "egg"
+];
+    const sauces=["chilly sauce","hot sauce"];
+    const mealType=["veg","non-veg"];
+    const course=["appetizer","starters","Maincourse","desserts"];
     const[meal,setMeal]= useState ({ 
         mealName: "",
         ingr1:"",
         ingr2:"",
         type:"",
         price:"",
-        courseMeal:""
-    })
+        courseMeal:"",
+        
+    });
     function Continue(e) {
         e.preventDefault()
         console.log(meal)
@@ -27,75 +38,97 @@ function CreateMeal() {
             console.log(res);
           })
           .catch((err) => console.log(err));
+          setMeal({
+            mealName:"",
+            ingr1:"Chicken",
+            ingr2:"chilly sauce",
+            type:"veg",
+            price:"",
+            courseMeal:"appetizer",
+
+          });
       }
     return (
-        <body>
+        
         <div>   
-        <form onClick={(e) => Continue(e)}>
+        <form onSubmit={(e) => Continue(e)}>
             
            <h1><b>CREATE YOUR MEAL!</b></h1><br/>
-            <label> 
-            {" "}
-            Meal Name: {" "}</label><br/>
+            <div>
+           <label> Meal Name: </label>
             <input type="text" name={meal.mealName}
-             onChange={(e) => setMeal({ ...meal,mealName: e.target.value })}
+             onChange={(e) => setMeal({ ...meal,mealName: e.target.value }) }
               required/>
-            <br/>
+        </div>
+        <br/>
+        <div>
+            <label> Main Ingredient:</label>
+        <select value={meal.ingr1}
+        onChange={(e) => setMeal({ ...meal,ingr1: e.target.value }) }
+            >
+                {ingredientList.map((option,index)=>(
+                    <option value={option} key={index}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+        <br/>
 
-            <label>
-                {" "}
-                Main Ingredient: {" "}</label><br/>
-            <select name={meal.ingr1} value={meal.ingr1}>
-                <option>Chicken</option>
-                <option>Mutton</option>
-                <option>Paneer</option>
-                <option>Fish</option>
-                <option>Egg</option>
-                <option>Mushroom</option>
-            onChange={(e) => setMeal({ ...meal,ingr1: e.target.value })}
-            </select><br/>
+        <div>
+            <label>Sauce:</label>
+        <select value={meal.ingr2}
+        onChange={(e) => setMeal({ ...meal,ingr2: e.target.value }) }
+            >
+                {sauces.map((option,index)=>(
+                    <option value={option} key={index}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+        <br/>
 
-            <label>
-                {" "}
-                Sauce:{" "}</label><br/>
-            <select name={meal.ingr2}>
-                <option>Chilly sauce</option>
-                <option>Schezwan sauce</option>
-                <option>Sweet chilly sauce</option>
-                onChange={(e) => setMeal({ ...meal,ingr2: e.target.value })}
-            </select><br/>
-             
-            <label>
-                {" "}
-                Type of meal:{" "}</label><br/>
-               <select name={meal.type}>
-                <option>Veg</option>
-                <option>Non-Veg</option>
-                onChange={(e) => setMeal({ ...meal,type: e.target.value })}
-               </select><br/>
-            
-            <label>
-                {" "}
-                Course Meal:{" "}</label><br/>
-               <select name={meal.courseMeal}>
-                <option>Appetizers</option>
-                <option>Starters</option>
-                <option>Main course</option>
-                <option>Dessert</option>
-                onChange={(e) => setMeal({ ...meal,courseMeal: e.target.value })}
-               </select><br/>
+        <div>
+        <label>Type:</label>
+        <select value={meal.type}
+        onChange={(e) => setMeal({ ...meal,type: e.target.value }) }
+            >
+                {mealType.map((option,index)=>(
+                    <option value={option} key={index}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+        <br/>
 
-            <label>
-                {" "}
-                Price:{" "}</label><br/>
-            <input type="number" name={meal.price} required/><br/>
-            <button type="submit" value="Submit">Continue</button> 
-            
+        <div>
+        <label>Course meal:</label>
+        <select value={meal.courseMeal}
+        onChange={(e) => setMeal({ ...meal,courseMeal: e.target.value }) }
+            >
+                {course.map((option,index)=>(
+                    <option value={option} key={index}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+       </div>
+       <br/>
+       <div>
+       <label>Price:</label>
+        <input type="number" name={meal.price} 
+        onChange={(e) => setMeal({ ...meal,price: e.target.value }) }
+        required/>
+        </div>
+        <button type="submit" value="Submit">
+            Continue
+        </button>
         </form>
         </div>
         
-        </body>);
+        );
   }
 
-export default CreateMeal;
-  
+//export default CreateMeal;
